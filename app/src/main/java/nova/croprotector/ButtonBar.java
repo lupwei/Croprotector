eeb06e400c0004d149e59bf785fe109950949876
Wonto <369418398@qq.com>
2017-07-28 19:24:02 +0800

    wt's UI

--- app/src/main/java/nova/croprotector/ButtonBar.java
+++ app/src/main/java/nova/croprotector/ButtonBar.java
@@ -1,7 +1,6 @@
 package nova.croprotector;
 
 import android.content.Context;
-import android.support.annotation.IdRes;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
