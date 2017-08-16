56aff871baf2fc6270f21dca6e74d65d9b5ae6c8
lupwei <lupengwei97@gmail.com>
2017-08-16 18:51:08 +0800

    permissions request improved

--- app/src/main/java/nova/croprotector/MainActivity.java
+++ app/src/main/java/nova/croprotector/MainActivity.java
@@ -5,10 +5,12 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -46,16 +48,39 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     private User_fragment userFragment;
     private FragmentManager fManager;
 
+    private final String[] PERMISSIONS=new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
+            Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
+            Manifest.permission.READ_PHONE_STATE, Manifest.permission.CAMERA,
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_DENIED) {
+            // When the compile and target version is higher than 22, please request the
+            // following permissions at runtime to ensure the
+            // SDK work well.
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
+            }
+
+        }
+
         // When the compile and target version is higher than 22, please request the
         // following permissions at runtime to ensure the
         // SDK work well.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
                             Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
@@ -66,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                             Manifest.permission.READ_PHONE_STATE, Manifest.permission.CAMERA
                     }
                     , 1);
-        }
+        }*/
         //Initialize DJI SDK Manager
         mHandler = new Handler(Looper.getMainLooper());
         DJISDKManager.getInstance().registerApp(this, mDJISDKManagerCallback);
