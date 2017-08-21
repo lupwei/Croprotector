dc620e92d6bea57f666c008b1b1c38744307a159
Sam <zthzhuren@126.com>
2017-08-21 15:42:16 +0800

    don't know what change

--- app/src/main/java/nova/croprotector/regist_activity.java
+++ app/src/main/java/nova/croprotector/regist_activity.java
@@ -9,18 +9,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+
+
+import okhttp3.OkHttpClient;
+
 //注册活动；
 public class regist_activity extends AppCompatActivity {
+
+    private EditText username_edit;
+    private EditText password_edit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.regist_layout);
 
         Button complete = (Button) findViewById(R.id.complete);
+        username_edit=(EditText)findViewById(R.id.username_edit);
+        password_edit=(EditText)findViewById(R.id.password_edit);
+
         complete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity.actionStart(regist_activity.this);
+                if(v.getId()==R.id.complete) {
+                    String phonenumber = username_edit.getText().toString();
+                    String password=password_edit.getText().toString();
+                    sendRequest(phonenumber,password);
+                    MainActivity.actionStart(regist_activity.this);
+                }
             }
         });
     }
@@ -28,5 +45,14 @@ public class regist_activity extends AppCompatActivity {
         //活动启动器
         Intent intent=new Intent(context,regist_activity.class);
         context.startActivity(intent);
+    }
+
+    private void sendRequest(String phonenumber,String password){
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                OkHttpClient client=new OkHttpClient();
+            }
+        }).start();
     }
 }
